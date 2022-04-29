3b9b5dfeb5f8761d2f6774ed82d718044e6bbfc4
theoklahuman <theoklahuman@gmail.com>
2022-04-29 16:11:48 -0500

    Make minor improvements to sketchpad

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const container = document.querySelector("#container");
-
 let div;
+const button = document.querySelector("button");
 
+/*
     for (let i = 0; i < 256; i++) {
         div = document.createElement("div");
         div.style.width = "20px";
         div.style.height = "20px";
         container.appendChild(div);
         hover(div);
+        clearPad(div);
     };
 
-    function hover(arg) {
-        arg.addEventListener("mouseover", function(event){
-            arg.style.background = "black";
+    */
+
+    function hover(div) {
+        div.addEventListener("mouseover", function(event){
+            div.style.background = "black";
         })
     }
+
+
+function clearPad(div) {
+    button.addEventListener("click", function(event) {
+        div.style.background = "white";
+    })
+}
+
+function makeGrid(num) {
+    for (let i = 0; i < (num * num); i++) {
+        div = document.createElement("div");
+        div.style.width = "10px";
+        div.style.height = "10px";
+        container.appendChild(div);
+        container.style.width = `${num * 10}px`;
+        container.style.height = `${num * 10}px`;
+        hover(div);
+        clearPad(div);
+}
+}
